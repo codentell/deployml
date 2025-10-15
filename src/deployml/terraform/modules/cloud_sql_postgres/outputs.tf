15a754e15ef4b6f0cb7999a5496e04e996115f47
GNitta <gtnitta@gmail.com>
2025-10-14 22:07:19 -0700

    "New fix for MLFlow Password Failures"

--- src/deployml/terraform/modules/cloud_sql_postgres/outputs.tf
+++ src/deployml/terraform/modules/cloud_sql_postgres/outputs.tf
@@ -16,7 +16,7 @@ output "db_public_ip" {
 }
 
 output "connection_string" {
-  value     = "postgresql+psycopg2://${var.db_user}:${random_password.db_password.result}@${google_sql_database_instance.postgres.public_ip_address}:5432/${var.db_name}"
+  value     = "postgresql+psycopg2://${var.db_user}:${urlencode(random_password.db_password.result)}@${google_sql_database_instance.postgres.public_ip_address}:5432/${var.db_name}"
 } 
 
 output "instance_connection_name" {
@@ -24,7 +24,7 @@ output "instance_connection_name" {
 }
 
 output "connection_string_cloud_sql" {
-  value = "postgresql+psycopg2://${var.db_user}:${random_password.db_password.result}@/mlflow?host=/cloudsql/${google_sql_database_instance.postgres.connection_name}"
+  value = "postgresql+psycopg2://${var.db_user}:${urlencode(random_password.db_password.result)}@/mlflow?host=/cloudsql/${google_sql_database_instance.postgres.connection_name}"
 }
 
 output "postgresql_credentials" {
@@ -35,24 +35,24 @@ output "postgresql_credentials" {
     db_name                  = var.db_name
     db_public_ip             = google_sql_database_instance.postgres.public_ip_address
     instance_connection_name = google_sql_database_instance.postgres.connection_name
-    connection_string        = "postgresql+psycopg2://${var.db_user}:${random_password.db_password.result}@${google_sql_database_instance.postgres.public_ip_address}:5432/${var.db_name}"
+    connection_string        = "postgresql+psycopg2://${var.db_user}:${urlencode(random_password.db_password.result)}@${google_sql_database_instance.postgres.public_ip_address}:5432/${var.db_name}"
   }
 }
 
 output "feast_connection_string" {
-  value = "postgresql+psycopg2://${var.db_user}:${random_password.db_password.result}@${google_sql_database_instance.postgres.public_ip_address}:5432/feast"
+  value = "postgresql+psycopg2://${var.db_user}:${urlencode(random_password.db_password.result)}@${google_sql_database_instance.postgres.public_ip_address}:5432/feast"
 }
 
 output "feast_connection_string_cloud_sql" {
-  value = "postgresql+psycopg2://${var.db_user}:${random_password.db_password.result}@/feast?host=/cloudsql/${google_sql_database_instance.postgres.connection_name}"
+  value = "postgresql+psycopg2://${var.db_user}:${urlencode(random_password.db_password.result)}@/feast?host=/cloudsql/${google_sql_database_instance.postgres.connection_name}"
 }
 
 output "metrics_connection_string" {
-  value = "postgresql+psycopg2://${var.db_user}:${random_password.db_password.result}@${google_sql_database_instance.postgres.public_ip_address}:5432/metrics"
+  value = "postgresql+psycopg2://${var.db_user}:${urlencode(random_password.db_password.result)}@${google_sql_database_instance.postgres.public_ip_address}:5432/metrics"
 }
 
 output "metrics_connection_string_cloud_sql" {
-  value = "postgresql+psycopg2://${var.db_user}:${random_password.db_password.result}@/metrics?host=/cloudsql/${google_sql_database_instance.postgres.connection_name}"
+  value = "postgresql+psycopg2://${var.db_user}:${urlencode(random_password.db_password.result)}@/metrics?host=/cloudsql/${google_sql_database_instance.postgres.connection_name}"
 }
 
 # Grafana-friendly connection URL using lib/pq style
